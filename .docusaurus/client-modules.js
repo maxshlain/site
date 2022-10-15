8f34a5a1409bf8dba21388160e4f24a057ae08cf
maxshlain <maxshlain@gmail.com>
2022-10-15 18:26:29 +0300

    stop indexing .docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/maximshlain/Code/GitHub/maxshlain/site/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/maximshlain/Code/GitHub/maxshlain/site/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/maximshlain/Code/GitHub/maxshlain/site/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/maximshlain/Code/GitHub/maxshlain/site/src/css/custom.css'),
-];
